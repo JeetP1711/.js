9f4f2a1adfd0cbe03f08876c17129bc0461370f1
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 17:37:16 +0530

    u4

--- Video 55/script.js
+++ Video 55/script.js
@@ -1,8 +1,10 @@
 console.log("Hey this is video 55 of sigma web dev.")
 // This console means we would see this in console whenever I will visit the page
 // Check the console in inspect you will notice it. 
-var a=34;
-var b=45;
-var c="Jeet";
-console.log( a + b + c);
-console.log(typeof a, typeof b, typeof c)
+var a = 34;
+var b = 45;
+var c = "Jeet"; // This is the string variable which has been automatically described
+var _a = 23;
+console.log(a + b + c); //This will print 79Jeet.
+// This is the addition of the three variables that we created in the script and now this would be printed.
+console.log(typeof a, typeof b, typeof c); //This will print the types of all three variables which we set here and made.
