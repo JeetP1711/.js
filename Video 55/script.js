64e55666cdb2fb3ed631a1b9fec36bdc4d10cc54
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 17:41:19 +0530

    U5

--- Video 55/script.js
+++ Video 55/script.js
@@ -4,7 +4,9 @@ console.log("Hey this is video 55 of sigma web dev.")
 var a = 34;
 var b = 45;
 var c = "Jeet"; // This is the string variable which has been automatically described
-var _a = 23;
+var _a = 23; //This way you can name the variable, but u can't name the variable like "5a" or something like that
+var $a = "Vaidehi"; //You can name the variable this way too
+// But number can't come before the letter in the variable name
 console.log(a + b + c); //This will print 79Jeet.
 // This is the addition of the three variables that we created in the script and now this would be printed.
 console.log(typeof a, typeof b, typeof c); //This will print the types of all three variables which we set here and made.
