836e5335cfb0afc30495dae2c43101803a5583ba
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 14:40:09 +0530

    u5

--- Video 54/script.js
+++ Video 54/script.js
@@ -7,4 +7,7 @@ console.log("The code is running like a wow........");
 // Its helpful in debugging the errors and finding the bugs in it
 var a = prompt("Enter the number");
 // The var character is used to define a variable like we did in the c and cpp and all
-// And the prompt is used to show in the alert slider but it does give u in 
+// And the prompt is used to show in the alert slider but it does give u input section where u can input whatever you want in it.
+console.log("The inputted number is "+ a);
+// And this is the output in the console where it shows the value of a which is inputted by the user
+// The variable can be int and string too
