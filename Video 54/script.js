3bd744241f78238085c882da94f388749b1fbf46
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 14:35:06 +0530

    u4

--- Video 54/script.js
+++ Video 54/script.js
@@ -1,1 +1,10 @@
 alert("Hello World");
+// This will show the alert while reloading the web page and showing what is written in alert
+console.log("The code is running........");
+console.log("The code is also running........");
+console.log("The code is running like a wow........");
+// This will show the console outputs in the console while opening the inspect and going in the console
+// Its helpful in debugging the errors and finding the bugs in it
+var a = prompt("Enter the number");
+// The var character is used to define a variable like we did in the c and cpp and all
+// And the prompt is used to show in the alert slider but it does give u in 
