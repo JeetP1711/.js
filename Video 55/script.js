2191354c067b8558da00488dbe2b3521dd2167f8
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 17:20:11 +0530

    u3

--- Video 55/script.js
+++ Video 55/script.js
@@ -1,3 +1,8 @@
 console.log("Hey this is video 55 of sigma web dev.")
 // This console means we would see this in console whenever I will visit the page
-// Check the console in inspect you will notice it.
+// Check the console in inspect you will notice it. 
+var a=34;
+var b=45;
+var c="Jeet";
+console.log( a + b + c);
+console.log(typeof a, typeof b, typeof c)
