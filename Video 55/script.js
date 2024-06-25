f5dca5a469cef8e2817aaa2749d3bb359f94620c
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 17:48:43 +0530

    u7

--- Video 55/script.js
+++ Video 55/script.js
@@ -5,7 +5,7 @@ var a = 34;
 var b = 45;
 var c = "Jeet"; // This is the string variable which has been automatically described
 var _a = 23; //This way you can name the variable, but u can't name the variable like "5a" or something like that
-var $a = "Vaidehi"; //You can name the variable this way too
+var $a = "Vaidehi"; //You can name the variable this way too //Not allowed
 // But number can't come before the letter in the variable name
 console.log(a + b + c); //This will print 79Jeet.
 // This is the addition of the three variables that we created in the script and now this would be printed.
