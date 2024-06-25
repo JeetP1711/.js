843a61ff91819a6e0916aaf7eba4de8242238d3f
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 14:54:03 +0530

    U5

--- Video 54/script.js
+++ Video 54/script.js
@@ -11,3 +11,15 @@ var a = prompt("Enter the number");
 console.log("The inputted number is "+ a);
 // And this is the output in the console where it shows the value of a which is inputted by the user
 // The variable can be int and string too
+// Now the confirm keyword in the console
+var isTrue = confirm("Are you sure you want to leave this page and blast your laptop");
+// This will ask you for the confirmation, yes or no and will ask you.
+// Now lets put some condition for this in the code
+if(isTrue){
+    console.log("Your computer will blast. 🫡🫨🫨.");
+}
+// This if condition was for when the user agrees and confirms ok in the case, there we must write if(isTrue)
+else{
+    console.log("Your computer won't blast dw.......🫡🫡.");
+}
+// This condition is for the else when the user selects for the cancel option in this..........
