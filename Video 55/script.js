7fe5ae6651bb91b249d1cbcb9e405901ac0c9dbe
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-25 17:09:43 +0530

    u2

--- Video 55/script.js
+++ Video 55/script.js
@@ -1,1 +1,3 @@
 console.log("Hey this is video 55 of sigma web dev.")
+// This console means we would see this in console whenever I will visit the page
+// Check the console in inspect you will notice it.
