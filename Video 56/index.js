affae7dce74c3b5b026f88aaa5703988df477401
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-26 14:39:14 +0530

    U5

--- Video 56/index.js
+++ Video 56/index.js
@@ -1,12 +1,13 @@
 console.log("Hi I am conditional operator");
 let age = 5;
 let grace = 2;
-
-console.log(age+grace);
-console.log(age-grace);
-console.log(age*grace);
-console.log(age/grace);
-console.log(age**grace);
+// Now learning about the operators of js like there are in c/c++
+console.log(age+grace); // This is simple addition operator // Simple Addition // 7
+console.log(age-grace); // This is simple subtraction operator // Simple Subtraction // 3
+console.log(age*grace); // This is simple multiplication operator // Simple Multiplication // 10
+console.log(age/grace); // This is simple division operator // Simple Division (Quotient) // 2.5
+console.log(age**grace); // This is not simple, it is the exponential operator // It will print age^grace (5^2) // 25
+console.log(age%grace); // This is not simple, it is the modulo operator // It will print the remainder of the division of age and grace // 1
 if((age+grace) > 18){ //This is the condition like the same like we use c/c++ or any other language
     console.log("You can drive"); // If age is greater then 18 then it will print this
 }
