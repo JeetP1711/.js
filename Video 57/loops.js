86cef8689d1f21f4210750a84e35ddf9a4cba897
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-26 17:22:28 +0530

    U5

--- Video 57/loops.js
+++ Video 57/loops.js
@@ -1,3 +1,9 @@
 // This is the loops in javascript
 console.log("This is the script for the FOR LOOP......");
 // Let us print the loop for printing 1 - 100 
+let a = 1;
+
+for(let i = 0; i<100 ; i++){
+    console.log(a+i);
+}
+// This is the loop for printing 1 - 100
