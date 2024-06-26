38f8dc3eb30b01a58d73c9d6d88295fcdb0e507f
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-26 17:50:44 +0530

    U6

--- Video 57/loops.js
+++ Video 57/loops.js
@@ -8,3 +8,16 @@ for(let i = 0; i<100 ; i++){
 }
 // This is the loop for printing 1 - 100
 
+// Now let us see the forin loop......
+let obj = {
+    "Name":"Jeet",
+    "ID": 1017,
+    "Company": "RagBooms"
+}
+// Here we created a object and now we want to print it through the forin loop
+
+// The for in loop is used to fetch the data from the object and print it.....
+for (const key in obj) {
+        const element = obj[key];
+        console.log( element);
+}
