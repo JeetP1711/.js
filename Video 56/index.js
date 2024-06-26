77bf15a90f0b2d3f260599ba7cbc7b2e8db6c244
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-26 16:10:45 +0530

    Format update

--- Video 56/index.js
+++ Video 56/index.js
@@ -3,16 +3,16 @@ let age = 5;
 let grace = 2;
 let realAge = 0;
 // Now learning about the operators of js like there are in c/c++
-console.log(age+grace); // This is simple addition operator // Simple Addition // 7
-console.log(age-grace); // This is simple subtraction operator // Simple Subtraction // 3
-console.log(age*grace); // This is simple multiplication operator // Simple Multiplication // 10
-console.log(age/grace); // This is simple division operator // Simple Division (Quotient) // 2.5
-console.log(age**grace); // This is not simple, it is the exponential operator // It will print age^grace (5^2) // 25
-console.log(age%grace); // This is not simple, it is the modulo operator // It will print the remainder of the division of age and grace // 1
-if((age+grace) > 18){ //This is the condition like the same like we use c/c++ or any other language
+console.log(age + grace); // This is simple addition operator // Simple Addition // 7
+console.log(age - grace); // This is simple subtraction operator // Simple Subtraction // 3
+console.log(age * grace); // This is simple multiplication operator // Simple Multiplication // 10
+console.log(age / grace); // This is simple division operator // Simple Division (Quotient) // 2.5
+console.log(age ** grace); // This is not simple, it is the exponential operator // It will print age^grace (5^2) // 25
+console.log(age % grace); // This is not simple, it is the modulo operator // It will print the remainder of the division of age and grace // 1
+if ((age + grace) > 18) { //This is the condition like the same like we use c/c++ or any other language
     console.log("You can drive"); // If age is greater then 18 then it will print this
 }
-else{
+else {
     console.log("You cannot drive"); // Or else it will print this
 }
 // Assignment Operators
@@ -42,14 +42,14 @@ multiline
 comment
 */
 // The else if statements
-let a=0;
+let a = 0;
 
-if(a>=18){
+if (a >= 18) {
     console.log("You can drive.");
 }
-else if(a==0){
+else if (a == 0) {
     console.log("Are you kidding");
 }
-else{
+else {
     console.log("You cannot drive")
 }
