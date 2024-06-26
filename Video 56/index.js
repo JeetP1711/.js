82ede01ef011988efb32dc0fde4df54bf96c6042
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-26 14:30:15 +0530

    U4

--- Video 56/index.js
+++ Video 56/index.js
@@ -1,8 +1,15 @@
 console.log("Hi I am conditional operator");
-let age = 45;
-if(age > 18){
-    console.log("You can drive");
+let age = 5;
+let grace = 2;
+
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age**grace);
+if((age+grace) > 18){ //This is the condition like the same like we use c/c++ or any other language
+    console.log("You can drive"); // If age is greater then 18 then it will print this
 }
 else{
-    console.log("You cannot drive");
+    console.log("You cannot drive"); // Or else it will print this
 }
