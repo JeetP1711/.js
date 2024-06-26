2c3490dcae27796ddb73a5f7ee76a9ca891200e8
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-26 16:08:35 +0530

    U10

--- Video 56/index.js
+++ Video 56/index.js
@@ -41,4 +41,15 @@ a
 multiline
 comment
 */
-// 
+// The else if statements
+let a=13;
+
+if(a>=18){
+    console.log("You can drive.");
+}
+else if(a==0){
+    console.log("Are you kidding");
+}
+else{
+    console.log("You cannot drive")
+}
