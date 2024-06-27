0ba7e79b66fe05626b42106290657ec9a89348ce
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-27 14:02:38 +0530

    Func3

--- Video 58/functions.js
+++ Video 58/functions.js
@@ -1,10 +1,11 @@
 // This video is about the functions in the javascript......
+// Making a function for the name, means which ever name I give, then all the lines would include that name
 function nice(name){
-    console.log("Hey "+name+ ", you are good!");
+    console.log("Hey "+name+ ", you are good!"); //Including a string name here....
     console.log("Hey "+name+ ", you are nice!!");
     console.log("Hey "+name+ ", you are awesome...!");
     console.log("Hey "+name+ ", your t-shirt is jordar..!");
     console.log("Hey, my name is "+ name);
 }
-
-nice("Jeet");
+// Here we made this function with the name, so that whenever I will give a name for any it will print this lines including that name.....
+nice("Jeet"); //Calling the nice function for name = "Jeet"
