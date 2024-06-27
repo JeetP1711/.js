ce8144ba219ad00762ad80824b2ea29a17467509
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-27 16:51:35 +0530

    Strings 2

--- Video 60/strings.js
+++ Video 60/strings.js
@@ -8,3 +8,5 @@ console.log(a[2]) // Accessing the third character of the string here which is e
 console.log(a[3]) // Accessing the fourth character of the string here which is t
 console.log(a[4]) // Accessing the fifth character of the string here which is not possible and not present so it will show undefined in the terminal. JS is the most error less language, it will try its best to not show errors and resolve the errors by itself as fast and perfect as possible.......
 console.log(a.length); // Printing the length of the a string
+// Template Literals
+console.log("Hi my name is "+ a);
