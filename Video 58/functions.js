3f556feff1e97c8d300ed05a4bbcda106a284edd
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-27 13:50:58 +0530

    Func2

--- Video 58/functions.js
+++ Video 58/functions.js
@@ -1,1 +1,10 @@
 // This video is about the functions in the javascript......
+function nice(name){
+    console.log("Hey "+name+ ", you are good!");
+    console.log("Hey "+name+ ", you are nice!!");
+    console.log("Hey "+name+ ", you are awesome...!");
+    console.log("Hey "+name+ ", your t-shirt is jordar..!");
+    console.log("Hey, my name is "+ name);
+}
+
+nice("Jeet");
