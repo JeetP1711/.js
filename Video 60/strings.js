acc3eae573f1ff2015705854732fcab5b6197afd
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-27 16:42:44 +0530

    Strings in js

---
+++ Video 60/strings.js
@@ -0,0 +1,12 @@
+console.log("This is string tutorials..");
+let a = "Jeet";
+
+console.log(a); // Simply Printing the a string
+console.log(a[0]) // Accessing the first character of the string here which is J
+console.log(a[1]) // Accessing the second character of the string here which is e
+console.log(a[2]) // Accessing the third character of the string here which is e
+console.log(a[3]) // Accessing the fourth character of the string here which is t
+console.log(a[4]) // Accessing the fifth character of the string here which is not possible and not present so it will show undefined in the terminal. JS is the most error less language, it will try its best to not show errors and resolve the errors by itself as fast and perfect as possible.......
+console.log(a.length); // Printing the length of the a string
+console.log(a.toLowerCase()); // Converting the a string into lowercase and then printing
+console.log(a.toUpperCase()); // Converting the a string into uppercase and then printing
