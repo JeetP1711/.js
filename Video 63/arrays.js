9559a0e018ebeae25833e3f6b8175784c56d662e
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-28 13:49:18 +0530

    Changing array element value

--- Video 63/arrays.js
+++ Video 63/arrays.js
@@ -2,10 +2,11 @@
 // Arrays are a collection of different datatypes in here javascript.....
 // Arrays are variables which can store more than one value.
 // And the values can also be of different types and data types like bool, var and string in one array...
+// Arrays are mutable
 let arr = [1,2,3,4,5,6,7];
 // Index   0,1,2,3,4,5,6
 console.log(arr);
-console.log(arr.length);
+console.log("Length of Array-> ",arr.length);
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
@@ -13,3 +14,6 @@ console.log(arr[3]);
 console.log(arr[4]);
 console.log(arr[5]);
 console.log(arr[6]);
+// I can even change the arrays value in here
+arr[0] = 6999;
+console.log("Changed value of arr[0] -> ",arr[0]);
