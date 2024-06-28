5b23d70c1c9b78885538210658e40123432132c9
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-28 16:40:10 +0530

    Array returning wit squared values

---
+++ Video 63/mfr.js
@@ -0,0 +1,8 @@
+let arr = [1,13,45,5,7,11];
+let newArr = [];
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    newArr.push(element**2);
+}
+console.log(newArr);
+// Here, we created a loop which will return the value of array but in squared form.......
