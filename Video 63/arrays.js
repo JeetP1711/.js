98af79536980455a7f8753c41756dded4c576602
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-28 13:43:20 +0530

    Printing Arrays

--- Video 63/arrays.js
+++ Video 63/arrays.js
@@ -2,3 +2,14 @@
 // Arrays are a collection of different datatypes in here javascript.....
 // Arrays are variables which can store more than one value.
 // And the values can also be of different types and data types like bool, var and string in one array...
+let arr = [1,2,3,4,5,6,7];
+// Index   0,1,2,3,4,5,6
+console.log(arr);
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
