86d88652b1fcb9ff1a7fcf58878e56d50bc78133
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-28 15:45:32 +0530

    Loops.js

--- Video 63/arrays.js
+++ Video 63/arrays.js
@@ -3,10 +3,10 @@
 // Arrays are variables which can store more than one value.
 // And the values can also be of different types and data types like bool, var and string in one array...
 // Arrays are mutable
-let arr = [1,2,3,4,5,6,7];
+let arr = [1, 2, 3, 4, 5, 6, 7];
 // Index   0,1,2,3,4,5,6
 console.log(arr);
-console.log("Length of Array-> ",arr.length);
+console.log("Length of Array-> ", arr.length);
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
@@ -16,13 +16,13 @@ console.log(arr[5]);
 console.log(arr[6]);
 // I can even change the arrays value in here
 arr[0] = 6999;
-console.log("Changed value of arr[0] -> ",arr[0]);
+console.log("Changed value of arr[0] -> ", arr[0]);
 // In console, type of array is considered as the object..
-console.log("Type of Array arr-> ",typeof arr);
+console.log("Type of Array arr-> ", typeof arr);
 
 // I can even convert this array into the string
 // By using the toString property in JS
-console.log("Converting the array into string-> ",arr.toString()); // By this the array is now converted into the string...
+console.log("Converting the array into string-> ", arr.toString()); // By this the array is now converted into the string...
 console.log(arr.join(" and "));
 // The join func also converts the array into to string and even more instead of , we can separate this string elements by whatever we want, like above i have converted the , into and, so the elements are separated by and......
 // Now lets talk about some functions which are important like push(), pop(), shift(), unshift().....
@@ -33,13 +33,13 @@ console.log(arr.join(" and "));
 // arr.shift(); // This will remove the element from front with index 0, which is 1 here..., means it is like pop but from front index 1 // First index removed
 // arr.unshift(10); // This will add the element 10 at the front with index[0], means it is like push but from front.... // First index added
 // Now lets learn about concat in arrays
-let a1 = [1,2,3];
-let a2 = [4,5,6];
-let a3 = [9,8,7];
+let a1 = [1, 2, 3];
+let a2 = [4, 5, 6];
+let a3 = [9, 8, 7];
 // I have taken this three arrays
 // Now let's concat this three arrays
 // console.log(a1.concat(a2,a3)); // Here I have concated values of a2 and a3 in a1, and this way both a2 and a3 are here concated in a1....
 // And the concat doesn't change the value of arrays here
 // Sort in js
-// a3.sort(); // This will sort the whole a3 array to 7,8,9 
+// a3.sort(); // This will sort the whole a3 array to 7,8,9
 // Sort function is used to sort the array
---
+++ Video 63/loops.js
@@ -0,0 +1,6 @@
+let a = [93,12,6,57,8,9,45]
+
+for (let i = 0; i < a.length; i++) {
+    const element = a[i];
+    console.log(element)
+}
