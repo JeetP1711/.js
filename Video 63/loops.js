41cb3b504f5cfb4adc35bff38af549cfe1045f3e
Jeet Patel <jeetspatel171104@gmail.com>
2024-06-28 16:02:47 +0530

    Loops

--- Video 63/loops.js
+++ Video 63/loops.js
@@ -1,6 +1,24 @@
-let a = [93,12,6,57,8,9,45]
+let ar = [93, 12, 6, 57, 8, 9, 45]
 
-for (let i = 0; i < a.length; i++) {
-    const element = a[i];
+for (let i = 0; i < ar.length; i++) {
+    const element = ar[i];
     console.log(element)
+}
+
+ar.forEach((value, index, arr) => {
+    console.log(value, index, arr);
+});
+
+// forin loop
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key, " : ", element);
+    }
 }
