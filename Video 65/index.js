7e51e9a2806d3b22b60ac783f7b0a9edbd951f38
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-01 09:36:13 +0530

    U1

---
+++ Video 65/index.js
@@ -0,0 +1,11 @@
+// This is the exercise where we will make a factorial calculator....
+var num = 5;
+var i = 1;
+var fac = 1;
+ while(i <= num) {
+    
+    fac = fac * i;
+    i++;
+ }
+
+ console.log(fac);
