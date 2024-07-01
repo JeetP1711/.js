3802fe2bf51900627567ecea6e1a7dafe042188d
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-01 12:36:54 +0530

    Exercise 1

--- Video 65/index.js
+++ Video 65/index.js
@@ -1,5 +1,5 @@
 // This is the exercise where we will make a factorial calculator....
-var num = 5;
+var num = 6;
 var i = 1;
 var fac = 1;
  while(i <= num) {
@@ -9,3 +9,12 @@ var fac = 1;
  }
 
  console.log(fac);
+
+//  Using reduce
+let arr = [1,2,3,4,5];
+// Here we created a function named sum which will return the value of sum in here
+function sum(a,b){ // Taking 2 values a & b
+   return a*b;
+}
+// And now printing the 
+console.log(arr.reduce(sum));
