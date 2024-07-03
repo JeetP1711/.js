6d9cf8d7ae8a27896e947159cec804be303ce9a1
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-03 16:20:46 +0530

    Arrays exp-01

--- Video 63/Arrays Exp.js
+++ Video 63/Arrays Exp.js
@@ -1,1 +1,11 @@
-// What are Arrays?
+/*
+What are Arrays?
+Arrays are the data type which can hold more that one data type in it, like it can hold a string, a number and a bool value at same time in one array.
+Like I can store a number, a string, and a bool in one same array without any hesitation in here...
+
+*/
+let arr = [1,2,3,4,5,6,7];
+
+console.log(arr); // This will print the whole array in here
+console.log(arr.length); // This will print the length of the array in here
+console.log(arr[1]); // This way I can even print the specific element of the array by giving the index number with the array name......
