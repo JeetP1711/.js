eea1123d3bfcb4527e1d67c6da5be12788d4b4e4
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-03 15:03:47 +0530

    Factorial using js

--- Exercises/Exercise 03/Factorial using loops.js
+++ Exercises/Exercise 03/Factorial using loops.js
@@ -1,9 +1,9 @@
-let fac = 1;
-let i = 1;
-let a = 6;
+let fac = 1; // Declared fac and assigned its value of 1
+let i = 1; // Declared value of i to 1
+let a = 6; // Declared value of a which to be declared here for the factorial calculation..........
 
-while(i<=6){
+while(i<=a){
     fac = fac * i;
-    i++;
+    i++; // Increasing the value of i and checking till it is equal to value of a
 }
-console.log(fac);
+console.log(fac); // Outputting the value of factorial
