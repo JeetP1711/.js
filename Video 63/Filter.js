b7da4a79c5fc9a25604c5c77c6efa840fb260e63
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-04 15:45:56 +0530

    Filters in JS

--- Video 63/Filter.js
+++ Video 63/Filter.js
@@ -9,3 +9,10 @@ let greater_than_7 = ((e)=>{
 }) // I created a function here which returns value true when the number is greater than seven, or else it will return false
 
 console.log(array.filter(greater_than_7)); // I am referencing the array with the greater_than_7 function above I made and if the element is greater than 7 than only the numbers would be shown, coz of true, false.......
+// I can even write whole the function in the filter bracket like...
+// console.log(array.filter(((e)=>{
+//     if(e>7){
+//         return true;
+//     }
+//     return false;
+// }))); // I can even write this way but it would be more complicated and hard to understand that's why we specify the whole function from here.......
