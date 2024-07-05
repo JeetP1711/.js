3ac6dc7c24f35b644fb35e1b0b96de7bbdb9c7f5
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-05 18:27:10 +0530

    Node Exp-03

--- Video 67/DOM Nodes.js
+++ Video 67/DOM Nodes.js
@@ -1,2 +1,5 @@
 console.log("Hello");
 // console.log(document.body.childNodes);
+// document.childNodes // This will give me list of all the child nodes present in the body like all the elements, even the spaces will be considered as the text nodes
+// And the output of this in console is --> NodeList(5) [text, div.container, text, script, text]
+// document.childNodes[0] //  This will give me the first element, of the html body, means it will return the text which is the space and enter we gave in the starting before the div.container in the body
