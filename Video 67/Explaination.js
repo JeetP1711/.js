03991da70538e0a47b02e98fd52cf9225880124e
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-05 17:39:46 +0530

    Nodes Exp-01

---
+++ Video 67/Explaination.js
@@ -0,0 +1,9 @@
+/*
+<div class="container">    // This will be considered as a text node element in the 
+        <div class="box">Box 1</div>
+        <div class="box">Box 2</div>
+        <div class="box">Box 3</div>
+        <div class="box">Box 4</div>
+        <div class="box">Box 5</div>
+    </div>
+*/
