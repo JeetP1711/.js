16dbfa29b425da4f01347056445eb59780ba124e
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-05 17:59:49 +0530

    Node Exp-02

--- Video 67/Explaination.js
+++ Video 67/Explaination.js
@@ -1,9 +1,12 @@
 /*
 <div class="container">    // This will be considered as a text node element in the 
+        <!-- This is a comment -->   // This will be considered as the comment node // Then again the text node coz of extra space.....
         <div class="box">Box 1</div>
         <div class="box">Box 2</div>
         <div class="box">Box 3</div>
         <div class="box">Box 4</div>
         <div class="box">Box 5</div>
     </div>
+
+    NodeList(5) [text, div.container, text, script, text] // This will
 */
