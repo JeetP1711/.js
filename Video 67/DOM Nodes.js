74864732c974e4ebdb353d28274aedabef9954a3
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-08 13:29:45 +0530

    Node Exp-01

--- Video 67/DOM Nodes.js
+++ Video 67/DOM Nodes.js
@@ -29,16 +29,16 @@ console.log("Hello");
 
 // The explanation of the DOM nodes in here is..............
 /*
-<div class="container">    // This will be considered as a text node element in the 
+<div class="container">
         <!-- This is a comment -->   // This will be considered as the comment node // Then again the text node coz of extra space.....
-        <div class="box">Box 1</div>
-        <div class="box">Box 2</div>
-        <div class="box">Box 3</div>
-        <div class="box">Box 4</div>
-        <div class="box">Box 5</div>
+        <div class="box">Box 1</div> // This will be considered as an element and will be as a element or say block type node [coz it's a block element]
+        <div class="box">Box 2</div> // This will be considered as an element and will be as a element or say block type node [coz it's a block element]
+        <div class="box">Box 3</div> // This will be considered as an element and will be as a element or say block type node [coz it's a block element]
+        <div class="box">Box 4</div> // This will be considered as an element and will be as a element or say block type node [coz it's a block element]
+        <div class="box">Box 5</div> // This will be considered as an element and will be as a element or say block type node [coz it's a block element]
     </div>
 
-    NodeList(5) [text, div.container, text, script, text] // This will
+    NodeList(5) [text, div.container, text, script, text] // This will be shown when i try to print the document.body, coz the div.container has 5 diff div, but they are in it, so it will be first stated as the div.container only......
     // firstChild etle ki je b pelo child hase akha html file no, jem ki ahiya return karse ae text form, karanke ahiya je apde extra space and enter space apel che ae ahiya text return karse, karanke je empty spaces hoy ae html ma as a text element ganay che, coz it refers as text.........
     // .firstChild--> This gives the first child of the html body
     // .lastChild--> This gives the last child of the html body
@@ -47,5 +47,17 @@ console.log("Hello");
     // Now if I write document.childNodes[1].childNodes ----->> then It will return me the elements of the div.container
     // Like aa nested jevu che, ke ek element na andar na child elements na childElements, and ahiya badha batavse, like ahiya apde je tree structure kaiye ne evu tree struct che, and eni jevi ritna aa akhu agad jaay and vadhe and u can track elements such way....
 
-    // FirstElementChild ----> aa vastu koi b element 
+    // FirstElementChild ----> aa vastu koi b element hase ne jem ki div, para or something like than even je b tag hoy specific ene element ganay, ekla text ne text aj ganay and baki badha tags ne to aa element tarikej le and ene ae select karse.
+    // So elementChild etle je b body ma element hase ene print karse, jemki agar hu evu lakhu ke document.body.firstElementChild ---> To aa div.container ne lavse, na ki text ke comment ne coz bane vastu element ma na ave, ae vastu text node maj ganay and aa div to ek particular element che etle ene print karyu and ave mare div.container na b agar element child joiye to hu ena b elementChild lakhis to ae div nej point karse, coz tya b text ne text aj ganse na ke koi element.
+    // So, elementChild matlab eva ki je proper element point karse..
+    // Same way agar hu .firstElementChild; ---> Prints first child of whatever selected..... // This will return me the div.container for the given index.html
+    // Same way agar hu .lastElementChild; ---> Prints last child of whatever selected..... // This will return me the script tag in this particular index.html
+
+    // And pachi ave hu aa je select karela element che ena jode kai b kari saku, like eni style change kari saku ke kai b, ae ave maru che to hu kari saku
+    // Like if hu document.body.firstElementChild.style.color="red"; // Hu avu lakhu to ae element no color red kari dese.... // Means ke ahiya div.container no color red thai jase badha div na color red thai jase............. 
+
+
+
+
+
 */
