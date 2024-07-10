42c6d0051fa754a19fa83d1ba296c2942b71fa8b
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-10 13:57:53 +0530

    Selecting By Id

--- Video 68/script.js
+++ Video 68/script.js
@@ -12,10 +12,4 @@ console.log("Hello");
 // If I want to specify the box3 only, then I need to get that element the id, and then whatever I add or remove that element would only get the style..
 // So for selecting specific element we take the id for it...
 let a = document.getElementById("box3"); // Here I have selected the element by the id box3 and stated that in the variable a
-// This ids and class works as the normal classes and ids like we give in the html and css
-// Class for multiple elements and id for the specific element...
-// But.........⬇️⬇️
-// There's a difference in the syntax of both id and class 
-// For id ---> document.getElementById // We write here **Element** just element
-// For class ---> document.getElementsByClassName // We write here **Elements** elements, coz a class has multiple elements in one class
 a.style.backgroundColor = "yellow"; // Styling the selected variable and giving it the BgColor yellow
