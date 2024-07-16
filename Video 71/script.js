9178563144e256909ecb2b7f3823aab8b5327757
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-16 13:24:54 +0530

    .after Method

--- Video 71/script.js
+++ Video 71/script.js
@@ -92,3 +92,15 @@ document.querySelector(".container").before(div3);
 // div2.innerHTML = "I am been inserted <b>by Jeet</b>"; // Aano matlab ke ama hu aa add karis and lakhis aa andar
 // div2.setAttribute("class","created"); // Ene apde class apyu .setAttribute thi ----> setAttribute("class","created") ---> etle peli value batave ke apde su apiye che ke su banaiye chiye ----> ahiya apde class banayu jene value api created
 // document.querySelector(".container").prepend(div2); // .prepend() ---> ano matlab ke aa body ma ekdum pehla add kari dese... matlab div.container ni upar
+
+
+// 4. After Method
+let div4 = document.createElement("div");
+div4.innerHTML = "I am been inserted <b>by Jeet</b>";
+div4.setAttribute("class", "created");
+document.querySelector(".container").after(div4);
+// Explaination of code....
+// let div2 = document.createElement("div"); // Aa line no matlab ki ahiya apde ek element banaiye che je ek div che...
+// div2.innerHTML = "I am been inserted <b>by Jeet</b>"; // Aano matlab ke ama hu aa add karis and lakhis aa andar
+// div2.setAttribute("class","created"); // Ene apde class apyu .setAttribute thi ----> setAttribute("class","created") ---> etle peli value batave ke apde su apiye che ke su banaiye chiye ----> ahiya apde class banayu jene value api created
+// document.querySelector(".container").prepend(div2); // .prepend() ---> ano matlab ke aa body ma ekdum chelle add kari dese... matlab div.container ni niche
