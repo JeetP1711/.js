100dbb81cffce6bf4cd3058d0f44d12d661630d1
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-17 14:15:56 +0530

    What is .addEventListener

--- Video 74/script.js
+++ Video 74/script.js
@@ -4,3 +4,12 @@
 // Like ek elements upar me click karyu, k dbl click karyu ke ene copy karyu aa badhi events kevay, nd aa badhi specific mouse events che
 // Mouse Events -----> https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events    // Aa link upar mouse ni badhi Events kbr padi jase
 // Keyboard Events ----> https://developer.mozilla.org/en-US/docs/Web/API/Element#keyboard_events // Aa link upar keyboard ni badhi events kbr padi jase
+
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",()=>{
+    document.querySelector(".box").innerHTML = "I am a <b>Big Box</b> now..";
+})
+// Ahiya .addEventListner no matlab ki ahiya hu ek evi event add karu chu, jenathi jyare b hu click karis tyare aa condition apply thase, jemki jyare hu aa btn par click karis tyare je arrow function ma lakhelu che ae apply thai jase and run thase
+// AddEventListener thi ama event add thase like jyare browser aa element par aa event listen karse tyare avu thase em....
