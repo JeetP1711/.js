1b226d6e1162cb0d3fb7fd6593f229889f3744e3
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-17 13:37:07 +0530

    What are Events?

--- Video 74/script.js
+++ Video 74/script.js
@@ -1,9 +1,6 @@
-// This Video Is Of EventBubbling, SetInterval & SetTimeout
-// Events means the things which are done on an element we have in here
-
-let btn = document.getElementById("btn");
-btn.addEventListener("click", ()=>{
-    document.querySelector(".box").innerHTML= "I am <b>UnBeatable</b> now...";
-})
-
-// Harek element ni different different events hoy jem ki ek button ne tame su su kari sako like hu button ne click kari saku, ena par double click kari saku and all and all, khasi badhi vastuo che
+// What are Events?
+// Actually Events are those which we do with the elements thorugh the mouse, keyboard or any way we interact with the elements in here...
+// Events means je b elements ahiya apde hoy ema apde alag alag vastu kariye eni sathe ene events kevay
+// Like ek elements upar me click karyu, k dbl click karyu ke ene copy karyu aa badhi events kevay, nd aa badhi specific mouse events che
+// Mouse Events -----> https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events    // Aa link upar mouse ni badhi Events kbr padi jase
+// Keyboard Events ----> https://developer.mozilla.org/en-US/docs/Web/API/Element#keyboard_events // Aa link upar keyboard ni badhi events kbr padi jase
