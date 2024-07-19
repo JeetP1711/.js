520ae7a72bd379b1f8d2c38f6f098a84425c35c9
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-19 15:51:50 +0530

    P-01

---
+++ Video 75/Promises-01.js
@@ -0,0 +1,8 @@
+// What are promises?
+// Promises matlab tuk ma ke evu ke apane assurity ape ke kaam thase to aa madse tamne, and kaam nai thay to aa malse
+// Promise matlab ya to solve thase ya to fail tthase but apane notificstion apse and madse ahiya.....
+// And ahiya apde banev lai sakiye means ya to resolve ya to error, or say rejected in the code...
+
+let p1 = new Promise((resolve, reject)=>{
+    resolve("This is resolved");
+})
---
+++ Video 75/script.js
@@ -0,0 +1,3 @@
+// This is the script for learning callbacks and promises
+
+// This firstly we are taking the  
