4d28856668183ca19c395e3fc42e36055bde35e2
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-19 18:11:59 +0530

    P2-02

--- Video 75/Promises-02.js
+++ Video 75/Promises-02.js
@@ -7,11 +7,8 @@ let p1 = new Promise(function(resolve, reject){
     setTimeout(() => {
         console.log("After 4 seconds P1");
         resolve("This promise is resolved");
-        console.log("This promise is resolved");
     }, 4000);
 })
-console.log(p1);
-
 p1.then(value=>{
     console.log(value)
 })
