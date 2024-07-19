20f3ead7623fccebd99fe85c72b6ad108ca19e03
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-19 16:06:46 +0530

    P-03

--- Video 75/Promises-01.js
+++ Video 75/Promises-01.js
@@ -3,10 +3,11 @@
 // Promise matlab ya to solve thase ya to fail tthase but apane notificstion apse and madse ahiya.....
 // And ahiya apde banev lai sakiye means ya to resolve ya to error, or say rejected in the code...
 
-let p1 = new Promise((resolve, reject)=>{
+let p1 = new Promise(function(resolve, reject){
     resolve("This is resolved");
 })
+// This is the basic syntax of a promise in here..
 
-p1.then(value=>{
-    console.log(value)
-})
+// Promise matlab ki ahiya me ek code lakhyo and agar ae resolve thase to mane aa print karine apse baki nai...
+
+// Ave mare ek advance ma samajiye thodu
