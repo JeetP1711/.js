df513a26d7645a877c13248ae74e8389307953d7
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-20 15:58:53 +0530

    P2-08

--- Video 75/Promises-02.js
+++ Video 75/Promises-02.js
@@ -2,45 +2,45 @@
 
 // Firstly I will just write down the code here...
 
-let p1 = new Promise(function(resolve, reject){
+let p1 = new Promise(function (resolve, reject) {
     console.log("Promise 1 is Pending...");
     setTimeout(() => {
         console.log("After 4 seconds P1");
         resolve("This promise is resolved");
     }, 4000);
 })
-p1.then(value=>{
+p1.then(value => {
     console.log(value)
 })
 
-let p2 = new Promise((resolve,reject)=>{
+let p2 = new Promise((resolve, reject) => {
     console.log("Promise is Pending waiting...");
     setTimeout(() => {
         console.log("After 4 seconds P2");
         reject(new Error("This is an error for the promise P2 with .catch(error)"))
     }, 4000);
 })
-p2.catch(error=>{
+p2.catch(error => {
     console.log(error);
 })
 
-let p3 = new Promise((resolve, reject)=>{
+let p3 = new Promise((resolve, reject) => {
     console.log("The promise is waiting...");
     setTimeout(() => {
         console.log("After 4 seconds P2");
         reject(new Error("This is an error passing this promise showing with .then(error)"));
     }, 4000);
 })
-p3.then((value)=>{
+p3.then((value) => {
     console.log(value);
-},(error=>{
+}, (error => {
     console.log(error);
 }))
 
 // Now explanation of each code in here....
 
 
-// let p1 = new Promise(function(resolve, reject){ // This is the basic syntax of a promise, and as explained in Promise-01.js ...... 
+// let p1 = new Promise(function(resolve, reject){ // This is the basic syntax of a promise, and as explained in Promise-01.js ......
 //     console.log("Promise 1 is Pending..."); // Here we are printing a basic console.log which shows the promise is still in the pending state at intial
 //     setTimeout(() => { // This is a setTimeOut function for the promise being resolved/rejected
 //         console.log("After 4 seconds P1"); // This is print for the after 4s....
@@ -64,8 +64,8 @@ p3.then((value)=>{
 // And ave niche apde aa error ne catch karisu
 //     }, 4000); // This is the 4s setTimeout function.....
 // })
-// p2.catch(error=>{ // This .catch means, ahiya apde value catch karisu error 
-// Catch means kai b hoy ae apde promise mathi eni value catch karisu like 
+// p2.catch(error=>{ // This .catch means, ahiya apde value catch karisu error
+// Catch means kai b hoy ae apde promise mathi eni value catch karisu like
 //     console.log(error); // This will print the catched error from the promise p2.. in here.... after the promise becomes rejected...
 // }) // And ahiya aa arrow function and puro function ahiya puro thayo
 // With this the reject promise with .catch() is finished here.......
