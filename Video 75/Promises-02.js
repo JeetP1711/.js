90883dfa4204f10a73b02af08faa315759edd8bc
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-20 14:21:56 +0530

    P2-05

--- Video 75/Promises-02.js
+++ Video 75/Promises-02.js
@@ -13,11 +13,22 @@ p1.then(value=>{
     console.log(value)
 })
 
+let p2 = new Promise((resolve,reject)=>{
+    console.log("Promise is Pending waiting...");
+    setTimeout(() => {
+        console.log("After 4 seconds P2");
+        reject(new Error("This is an error for the promise P2 with .catch(error)"))
+    }, 4000);
+})
+p2.catch(error=>{
+    console.log(error);
+})
+
 let p3 = new Promise((resolve, reject)=>{
     console.log("The promise is waiting...");
     setTimeout(() => {
         console.log("After 4 seconds P2");
-        reject(new Error("This is an error passing this promise"));
+        reject(new Error("This is an error passing this promise showing with .then(error)"));
     }, 4000);
 })
 p3.then((value)=>{
