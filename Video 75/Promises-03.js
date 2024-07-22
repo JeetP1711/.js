3e257161d5bddc9b7b1b3ca86619a77a20dbe1ae
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-22 14:22:19 +0530

    P3-05

--- Video 75/Promises-03.js
+++ Video 75/Promises-03.js
@@ -27,6 +27,17 @@ p1.then((value) => {
 }).then((value) => {
     console.log("We are done");
     return 2;
-}).then(value=>{
+}).then(value => {
     console.log("We are pakka vala done now....!");
 })
+
+// Explaination of the code above.....!
+
+// Explanation of code inline....
+
+// let p1 = new Promise((resolve, reject) => { // Creating a basic promise here with the name p1....
+//     console.log("This is the main promise...!"); // Consoling the basic in the promise for the refrence of us
+//     setTimeout(() => { // This is here the basic setTimeout function for the differentiation of our program...
+//         resolve("Main Promise Resolved After 2s...!"); // This is the resolve for the p1 promise...
+//     }, 2000); // And here the 2s timeout Interval for this....
+// }) // Here the promise p1 is finished..
