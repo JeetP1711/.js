6187f522c3ef2d5536320a2fdcb06beb64c9d427
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-22 14:15:48 +0530

    P3-04

--- Video 75/Promises-03.js
+++ Video 75/Promises-03.js
@@ -4,10 +4,29 @@
 // Like loop jevu ke ek pate pachi biju, bija pachi tijo and so on and on.....
 // Giving an example....--->
 
+// Promise Chaining no matlab ki je apde ek promise hoy ena attached biji khasi badhi promise hu extend kari saku and ek n pachad hu khasi badhi promises apply kari saku...
+// Means ek ni pachad biji, bija ni pachad tiji and so on and on and on
+// Agad mare ema gamme tetli nakhvi hoy etli nankhay...
 
-let p1 = new Promise((resolve,reject)=>{
+let p1 = new Promise((resolve, reject) => {
     console.log("This is the main promise...!");
     setTimeout(() => {
-        console.log("After 2s...!");
+        resolve("Main Promise Resolved After 2s...!");
     }, 2000);
+})
+
+p1.then((value) => {
+    console.log(value);
+    let p2 = new Promise((resolve, reject) => {
+        console.log("This is Second Promise...!");
+        setTimeout(() => {
+            resolve("After 2s in derived promise....!");
+        }, 2000);
+    })
+    return p2;
+}).then((value) => {
+    console.log("We are done");
+    return 2;
+}).then(value=>{
+    console.log("We are pakka vala done now....!");
 })
