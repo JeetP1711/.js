2d3fc3008e683c50a1f1447ebf0c8e838df69f24
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-30 13:24:42 +0530

    AsyncAwait01

--- Video 76/Async Await.js
+++ Video 76/Async Await.js
@@ -1,1 +1,22 @@
 // This is the file containing the async await...!
+// Let us try to fetch the data of the weather and I am trying to make the weather fetching console
+
+function getWeatherData(){
+    let GujaratWeather = new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve("21 Degree")
+        }, 1000);
+    })
+
+    let MumbaiWeather = new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve("27 Degree")
+        }, 7000);
+    })
+    GujaratWeather.then(value=>{
+        console.log(value)
+    })
+    MumbaiWeather.then(value=>{
+        console.log(value)
+    })
+}
