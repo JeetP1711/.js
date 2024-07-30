7d58c5004daff816afee08c19807922c311b00f4
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-30 14:54:25 +0530

    AsyncAwait04

--- Video 76/Async Await.js
+++ Video 76/Async Await.js
@@ -1,6 +1,8 @@
 // This is the file containing the async await...!
 // Let us try to fetch the data of the weather and I am trying to make the weather fetching console
 
+async function TheMain(){
+
 async function getWeatherData(){
     let GujaratWeather = new Promise((resolve,reject)=>{
         setTimeout(() => {
@@ -20,7 +22,24 @@ async function getWeatherData(){
     console.log("Fetching Mumbai Weather.... Please Wait!");
     let MumbaiW = await MumbaiWeather;
     console.log("Fetched Mumbai Weather: "+ MumbaiW);
+    return [GujaratW, MumbaiW]
 }
 
+function Rag(){
+    console.log("I am not waiting, coz I am the normal function without await and async");
+}
+
+async function Rag1(){
+    console.log("I am waiting, coz I am the function with await and async which means I will be loaded at last after all the promises are synced...!");
+}
+
+
+
 console.log("Welcome to Weather Forecast!!");
-getWeatherData();
+let b = Rag();
+let a = await getWeatherData();
+let c = await Rag1();
+}
+
+
+TheMain();
