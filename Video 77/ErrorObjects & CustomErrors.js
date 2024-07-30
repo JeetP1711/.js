12800fd6d880547ae8cbaa24b429e6a07ed283c4
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-30 17:18:20 +0530

    CustomError-10

--- Video 77/ErrorObjects & CustomErrors.js
+++ Video 77/ErrorObjects & CustomErrors.js
@@ -5,13 +5,13 @@
 // ANd error object means in the error, I just want particular thing or that to be printed.....
 
 
-try {
-    console.log(Jeet)
-} catch (error) {
-    console.log(error.name); // This will help me print the name and type of the error, like here it is the reference Error
-    console.log(error.message); // This will help me print the message of the error to be filled like here the message is "Jeet is not defined"
-    console.log(error.stack) // This will help me print the whole error in here.....
-}
+// try {
+//     console.log(Jeet)
+// } catch (error) {
+//     console.log(error.name); // This will help me print the name and type of the error, like here it is the reference Error
+//     console.log(error.message); // This will help me print the message of the error to be filled like here the message is "Jeet is not defined"
+//     console.log(error.stack) // This will help me print the whole error in here.....
+// }
 
 // Like ave hu emnem error lakhu to mane akhi error print karine ape like blah blah blah, vastu hoy nani but error akha gaam thi moti,, to ene seolve karva error object banayu che, so that ki ahiya mane bau ghucham ghucham na lage
 
