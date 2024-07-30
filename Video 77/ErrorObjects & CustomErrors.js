bc749c6469e777c2811e4c321fca0a6334afb5d7
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-30 17:05:53 +0530

    CustomError-01

--- Video 77/ErrorObjects & CustomErrors.js
+++ Video 77/ErrorObjects & CustomErrors.js
@@ -1,1 +1,12 @@
 // This is for the custom errors and Error Objects....!
+
+// The custom error is the way like I can throw a particular type of error from here in
+
+// ANd error object means in the error, I just want particular thing or that to be printed.....
+
+
+try {
+    console.log(Jeet)
+} catch (error) {
+    console.log(error.name); // This will help me print the name and type of the error, like here it is the reference Error
+}
