8b118abcdef1a4be0df184b321b587ada27a2ef6
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-30 17:06:01 +0530

    CustomError-02

--- Video 77/ErrorObjects & CustomErrors.js
+++ Video 77/ErrorObjects & CustomErrors.js
@@ -9,4 +9,5 @@ try {
     console.log(Jeet)
 } catch (error) {
     console.log(error.name); // This will help me print the name and type of the error, like here it is the reference Error
+    console.log(error.message); // This will help me print the message of the error to be filled like here the message is "Jeet is not defined"
 }
