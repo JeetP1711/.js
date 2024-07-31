dfebfa595fe1d3eb8530e14eda66ad90fe973030
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-31 15:56:13 +0530

    Finally-04

--- Video 79/Finally.js
+++ Video 79/Finally.js
@@ -1,15 +1,15 @@
 // The finally is a keyword used along in the try catch method in here....!
 // Which means ke jyare b aa try catch chalse tyare aa finally ma lakhelu statement ae run thai aj jase.....!
 
-try {
-    let sum = 5 + 7;
-    console.log(sum*X);
-} catch (error) {
-    console.log("This is an error!!!");
-}
-finally{
-    console.log("This is the final and last statement....!");
-}
+// try {
+//     let sum = 5 + 7;
+//     console.log(sum*X);
+// } catch (error) {
+//     console.log("This is an error!!!");
+// }
+// finally{
+//     console.log("This is the final and last statement....!");
+// }
 
 // The finally statement may seem like a normal statement means ke aa vastu error avse ke nai ave print thase, but to b aa imp che
 // Like apde normally console.log kri sakiye if normal hoy to, but aama ek catch che
