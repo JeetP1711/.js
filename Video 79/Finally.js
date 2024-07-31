30060ec91c465bb03b3b32e1e8a2554b0a9434b0
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-31 15:57:07 +0530

    Finally-09

--- Video 79/Finally.js
+++ Video 79/Finally.js
@@ -14,22 +14,22 @@
 // The finally statement may seem like a normal statement means ke aa vastu error avse ke nai ave print thase, but to b aa imp che
 // Like apde normally console.log kri sakiye if normal hoy to, but aama ek catch che
 
-function main1() {
-    try {
-        let sum = 5 + 7;
-        console.log(sum * X);
-        return true;
-    } catch (error) {
-        console.log("This is an error!!!");
-        return false;
-    }
-    finally{
-    console.log("This is the final and last statement....!");
-    }
-}
+// function main1() {
+//     try {
+//         let sum = 5 + 7;
+//         console.log(sum * X);
+//         return true;
+//     } catch (error) {
+//         console.log("This is an error!!!");
+//         return false;
+//     }
+//     finally{
+//     console.log("This is the final and last statement....!");
+//     }
+// }
 
-a = main1();
-console.log(a);
+// a = main1();
+// console.log(a);
 
 // Ahiya me ek function lidhu che and jyare hu finally hatai dau chu to aa console.log nathi thatu, baki finally lakhu to last statement print thay che
 // Etle aa vastu mate finally kaam ma ave, jyare apde koi function ni andar kai karva mangiye and all jyare kariye tyare apde finally clause karvani jrur pade
