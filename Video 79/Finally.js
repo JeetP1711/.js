0c7daaa5706f4ebb9eba26352940a4718d90bdb1
Jeet Patel <jeetspatel171104@gmail.com>
2024-07-31 15:55:26 +0530

    Finally-02

--- Video 79/Finally.js
+++ Video 79/Finally.js
@@ -1,2 +1,12 @@
 // The finally is a keyword used along in the try catch method in here....!
 // Which means ke jyare b aa try catch chalse tyare aa finally ma lakhelu statement ae run thai aj jase.....!
+
+try {
+    let sum = 5 + 7;
+    console.log(sum*X);
+} catch (error) {
+    console.log("This is an error!!!");
+}
+finally{
+    console.log("This is the final and last statement....!");
+}
