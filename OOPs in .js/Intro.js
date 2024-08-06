419272a57ddfbf1073ec643ee3fb9e5bccff49c7
Jeet Patel <jeetspatel171104@gmail.com>
2024-08-06 14:20:03 +0530

    OOPs-03

--- OOPs in .js/Intro.js
+++ OOPs in .js/Intro.js
@@ -6,3 +6,9 @@
 // 2. Abstraction
 // 3. Encapsulation
 // 4. Polymorphism
+
+// Explaining Each Pillar:
+// 1. Inheritance :
+// Inheritance means ke jema ek class thi bija class ne point thay and enathi apde ene refer karay and ene properties apde api sakiye and badhu kari sakiye....!
+// Official Defination Below
+// Inheritance is the concept in OOPs in which one class inherits the attributes and methods of another class. The class whose properties and methods are inherited is known as the Parent class. And the class that inherits the properties from the parent class is the Child class.
