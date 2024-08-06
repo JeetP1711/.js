0e496294e314d51b4907c3dc186f2a9bf4422604
Jeet Patel <jeetspatel171104@gmail.com>
2024-08-06 13:47:37 +0530

    OOPs-02

--- OOPs in .js/Intro.js
+++ OOPs in .js/Intro.js
@@ -1,2 +1,8 @@
 // What is OOPs?
 // Object-oriented programming (OOP) is a style of programming characterized by the identification of classes of objects closely linked with the methods (functions) with which they are associated.
+
+// Four Pillars Of OOPs
+// 1. Inheritance
+// 2. Abstraction
+// 3. Encapsulation
+// 4. Polymorphism
