3ff6f3c4d34c5e5a96da7d8ffe8daaf843575f9e
Jeet Patel <jeetspatel171104@gmail.com>
2024-08-06 14:34:10 +0530

    OOPs-04

--- OOPs in .js/Intro.js
+++ OOPs in .js/Intro.js
@@ -12,3 +12,8 @@
 // Inheritance means ke jema ek class thi bija class ne point thay and enathi apde ene refer karay and ene properties apde api sakiye and badhu kari sakiye....!
 // Official Defination Below
 // Inheritance is the concept in OOPs in which one class inherits the attributes and methods of another class. The class whose properties and methods are inherited is known as the Parent class. And the class that inherits the properties from the parent class is the Child class.
+
+// 2. Abstraction :
+// Abstraction means apde je vastu just apda sudhi rakhvi hoy and bija ne bataine kai faydo nai em, jem ki mare backend ma su chale che ae user bataine kai faydo nai to apde ae vastu just apdi sudhi simit rakhvani, like apde C++ ma public and private karta hata ae.....!
+// Official Defination Below
+// Abstraction, in the context of OOP, refers to the ability to hide complex implementation details and show only the necessary features of an object. This simplifies the interaction with objects, making programming more intuitive and efficient.
