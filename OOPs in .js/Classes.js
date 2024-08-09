bed3a81bce1df688562988fafb151705c45c7f5c
Jeet Patel <jeetspatel171104@gmail.com>
2024-08-09 14:40:38 +0530

    Classes-02

--- OOPs in .js/Classes.js
+++ OOPs in .js/Classes.js
@@ -3,3 +3,23 @@
 // Objects can just store the value from the user itself from the code and in it through many ways.
 // Uk in a class we can describe many things, class is like a big function or say the main func we made in the C and C++, its like that only, just we can create many functions like this in it too
 // Its like the classes in C++ only, just a lil syntax changed and like that............!
+
+// Syntax given below
+
+class FirstClass{
+    Greeting(){
+        console.log("Hi this is the first class...!")
+        console.log("Hi "+ this.name + " this is the first Class of JS... Hey!!!");        
+    }
+    Namaste(name){
+        this.name = name;
+    }
+}
+
+let Jeet = new FirstClass;
+let Raghvat = new FirstClass;
+
+Jeet.Namaste("Jeet");
+Jeet.Greeting();
+Raghvat.Namaste("Jeet");
+Raghvat.Greeting();
