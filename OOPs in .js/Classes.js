2d0b27a77c64d23c6940358dda1d71c888cafb62
Jeet Patel <jeetspatel171104@gmail.com>
2024-08-09 14:18:58 +0530

    Classes-01

--- OOPs in .js/Classes.js
+++ OOPs in .js/Classes.js
@@ -1,1 +1,5 @@
 // What are classes?
+// Classes are like the objects in which u can store anything and everything in there.
+// Objects can just store the value from the user itself from the code and in it through many ways.
+// Uk in a class we can describe many things, class is like a big function or say the main func we made in the C and C++, its like that only, just we can create many functions like this in it too
+// Its like the classes in C++ only, just a lil syntax changed and like that............!
