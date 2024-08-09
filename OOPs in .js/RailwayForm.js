b7ebc1808d15234aacfdda102b902086d0226039
Jeet Patel <jeetspatel171104@gmail.com>
2024-08-09 15:24:26 +0530

    Classes-05

--- OOPs in .js/RailwayForm.js
+++ OOPs in .js/RailwayForm.js
@@ -1,1 +1,13 @@
 // This is a railway form for submitting and cancelling the form, its like a basic form just a basic..............!
+
+class RailwayForm{
+    submit(){
+        console.log(this.name+ ": Your form is been submitted!!!");
+    }
+    cancel(){
+        console.log(this.name+ ": Your form is been cancelled");
+    }
+    fill(name){
+        this.name = name;
+    }
+}
