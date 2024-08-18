a2fa869d1580ab21dbfd1adec03d458086eb1c6f
Jeet Patel <157380070+JeetP1711@users.noreply.github.com>
2024-08-18 23:32:28 +0530

    Update tempCodeRunnerFile.js

--- Video 79/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-//     let sum = 5 + 7;
-//     console.log(sum*X);
-// } catch (error) {
-//     console.log("This is an error!!!");
-// }
-// finally{
-//     console.log("This is the final and last statement....!");
-// }
